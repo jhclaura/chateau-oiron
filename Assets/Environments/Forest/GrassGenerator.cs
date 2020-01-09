335b83639f44e54bddc8c3bd2caf43117d60d013
jhclaura <linkinmonkey@gmail.com>
2020-01-09 10:53:30 -0500

    Forest - add carpet grass

---
+++ Assets/Environments/Forest/GrassGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassGenerator : MonoBehaviour
+{
+    public GameObject grassPrefab;
+    public int amount;
+
+    void Start()
+    {
+        Vector3 randomPos;
+        for (int i=0; i<amount; i++)
+        {
+            randomPos = new Vector3(Random.Range(-0.3f, 0.3f), 0, Random.Range(0f, -21f));
+            GameObject newGrass = Instantiate(grassPrefab, randomPos, Quaternion.AngleAxis(Random.Range(0f, 180f), Vector3.up));
+            newGrass.transform.SetParent(transform, false);
+        }
+    }
+}
