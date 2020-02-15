765d4d95dd86ffc434047b46cf2b5eed25f8ec3c
jhclaura <linkinmonkey@gmail.com>
2020-02-15 12:15:53 -0500

    Film: add Fire + Forest + Beetle

---
+++ Assets/Film/Forest/Fire/FilmFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Film
+{
+    public class FilmFire : MonoBehaviour
+    {
+        public Mascot mascot;
+
+        public void ActivateMascot()
+        {
+            mascot.mascotAnimator.SetTrigger("StartWalking");
+            mascot.mascotAnimator.SetFloat("LegMove", 1f);
+        }
+
+        public void MascotContinueWalking()
+        {
+            mascot.TriggerAnimation("ContinueWalking");
+        }
+    }
+}
